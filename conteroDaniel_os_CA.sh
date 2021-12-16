028ad7039e331d61ded2b7a72f36ce22ba166737
Daniel Contero <daniel.contero@gmail.com>
2021-12-16 20:00:53 +0000

    multiple fixes to allow iteration

--- conteroDaniel_os_CA.sh
+++ conteroDaniel_os_CA.sh
@@ -10,13 +10,10 @@ checkAndCreateFile() {
 
     local file_path
     file_path=$1
-    if ! [ -f "$file_path" ]
+    if ! [ -e "$file_path" ]
     then
-        echo -e "       File couldn't be found, creating it with zero-lenght  $file_path"
-        #mkdir -p -- "${file_path%/*}" && touch -- "$file_path"
-        
-    else
-        echo -e "       This file already exist  $file_path"
+        echo -e "\t\tFile couldn't be found, creating it with zero-lenght  $file_path"
+        mkdir -p -- "${file_path%/*}" && touch -- "$file_path"
     fi
 }
 
@@ -28,7 +25,11 @@ fileCopy() {
 
     local final_dir file_name name_part version_part num_files final_path
 
-    if [ -f "$final_dir/$file_name" ]
+    file_name="$1"
+    final_dir="$2"
+    file_path="$3"
+
+    if [ -e "$final_dir/$file_name" ]
     then
         if [[ "$file_name" =~ .+\.[0-9]+ ]]
         then
@@ -36,24 +37,27 @@ fileCopy() {
             version_part="$(echo "$file_name" | grep -P '(?<=\.)[0-9]+' -o)"
             num_files=$(find "$final_dir" -name "$name_part*" | wc -l)
             num_files=$((++num_files))
-            if (( num_files > version_part ))
+            if (( num_files < version_part ))
             then
+                final_path="$final_dir/$name_part.$version_part"
+            else
                 final_path="$final_dir/$name_part.$num_files"
             fi
         else
             final_path="$final_dir/$file_name.1"
         fi
+        cp -f "$file_path" "$final_path"
+        echo -e "\t\t$file_path copied to $final_path"
     else
         if [[ "$file_name" =~ .+\.[0-9]+ ]]
         then
             final_path="$final_dir/$file_name"
+            cp -f "$file_path" "$final_path"
+            echo -e "\t\t$file_path copied to $final_path"
         else
-            final_path="$final_dir/$file_name.1"
+            fileCopy "$file_name.1" "$final_dir" "$file_path"
         fi
     fi
-    #cp "$file_path" "$final_path"
-    echo "$file_path copied to $final_path"
-    
 }
 
 fileComparing() {
@@ -63,20 +67,21 @@ fileComparing() {
 
     local file_path file_dir file_name final_dir  
     local num_files final_path inside_file user_name
-    file_path="$1"
     user_name="$2"
+    file_path="$1"
+
     if [ -f "$file_path" ]
     then
         file_dir=$(dirname "$file_path")
         file_name=$(basename "$file_path")
         final_dir="$EXTRACT_PATH/$user_name/${file_dir:2}"
-        #[ ! -e "$final_dir" ] && mkdir -p "$final_dir" && echo "Created $final_dir"
-        fileCopy 
+        [ ! -e "$final_dir" ] && mkdir -p "$final_dir" && echo "Created $final_dir"
+        fileCopy "$file_name" "$final_dir" "$file_path"
     elif [ -d "$file_path" ]
     then
         file_dir="$file_path"
         final_dir="$EXTRACT_PATH/$user_name/${file_dir:2}"
-        #[ ! -e "$final_dir" ] && mkdir -p "$final_dir" && echo "Created $final_dir"
+        [ ! -e "$final_dir" ] && mkdir -p "$final_dir" && echo "Created $final_dir"
         for inside_file in "$file_path"/*
         do
             fileComparing "$inside_file" "$user_name"
@@ -110,12 +115,12 @@ readBackupFile() {
             then
                 raw_path=".${line:2}"
             fi
-                checkAndCreateFile "$raw_path"
-                fileComparing "$raw_path" "$user_name"         
+            checkAndCreateFile "$user_path${raw_path:2}"
+            fileComparing "$raw_path" "$user_name"         
         done < "$file_path"
     else
-        #touch "$file_path"
-        echo -e "   $file_name file created in $file_path"
+        touch "$file_path"
+        echo -e "\t$file_name file created in $file_path"
     fi
 }
 
@@ -138,24 +143,25 @@ getFileContent() {
 compressBackup() {
     # Compress back the backup.tr.gz file after all the operations are completed
 
-    #tar -zcf "$backup_path" "$extract_path"
+    echo "STARTING BACKUP"
+    cd "$EXTRACT_PATH" && tar -zcvf "$BACKUP_PATH" . && cd - 
     echo "File $BACKUP_PATH is being compressed"
+    rm -r "$EXTRACT_PATH"
+    echo "$EXTRACT_PATH is being deleted"
 }
 
 backupExtract() {
-    # Checks if "/tmp/backup" exists (extraction path given) and creates the path
-    # if doesn't exist. Then checks if "/var/backup.tar.gz" exists, extracting 
-    # it on the given location, otherwise promnts msg couldnt be found.
+    # Cleans $EXTRACT_PATH, deleting it and creates the path for a fresh start
+    # Then checks if "/var/backup.tar.gz" exists, extracting it on the given
+    # location, otherwise promnts msg couldnt be found.
 
-    if ! [ -d "$EXTRACT_PATH" ]
-    then
-        #mkdir "$extract_path"
-        echo "$EXTRACT_PATH Path created"
-    fi
+    rm -r "$EXTRACT_PATH"
+    mkdir "$EXTRACT_PATH"
+    echo "$EXTRACT_PATH Path created"
 
     if [ -f "$BACKUP_PATH" ]
     then
-        #tar -zxf "$backup_path" -C "$extract_path"
+        tar -zxf "$BACKUP_PATH" -C "$EXTRACT_PATH"
         echo "$BACKUP_PATH is being extracted in $EXTRACT_PATH"
     else
         echo "$BACKUP_PATH couldn't be found"
@@ -172,6 +178,7 @@ while IFS= read -r line || [ -n "$line" ]
 do 
     [ -z "$line" ] && continue
     getFileContent "$line"
+    echo -e "\n"
 done < "$file_input"
 
 compressBackup
